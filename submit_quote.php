721f4877ec19a54fc224bcf400fb7cf4991ac057
Mahesh Tambe <maheshtambe5112@gmail.com>
2025-08-08 15:32:44 +0530

    Initial Commit

---
+++ submit_quote.php
@@ -0,0 +1,35 @@
+<?php
+// DB credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pixel_db";
+
+// DB connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get & sanitize input
+$name = htmlspecialchars($_POST['name']);
+$phone = htmlspecialchars($_POST['phone']); 
+$email = htmlspecialchars($_POST['email']);
+$requirement = htmlspecialchars($_POST['requirement']);
+
+// SQL insert
+$stmt = $conn->prepare("INSERT INTO quotes (name, phone, email, requirement) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $name, $phone, $email, $requirement);
+
+// Execute
+if ($stmt->execute()) {
+  echo "<script>alert('Message sent successfully!'); window.location.href='index.html';</script>";
+} else {
+  echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
